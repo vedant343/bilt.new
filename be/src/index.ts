8e4a89f0dd5d61ca34e308735012cf7b7a6a6111
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2025-04-29 21:51:17 +0530

    added engineered prompt

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,29 +2,72 @@ require("dotenv").config();
 import express from "express";
 import OpenAI from "openai";
 import cors from "cors";
+import { BASE_PROMPT, getSystemPrompt } from "./prompts";
+import { basePrompt as nodeBasePrompt } from "./defaults/node";
+import { basePrompt as reactBasePrompt } from "./defaults/react";
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   apiKey: process.env.OPENAI_API_KEY,
-
 });
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-async function main() {
-  const completion = await openai.chat.completions.create({
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
+  const response = await openai.chat.completions.create({
     model: "qwen/qwen3-30b-a3b:free",
     messages: [
       {
         role: "user",
-        content: "Write a code for todo application ",
+        content: prompt,
       },
     ],
     temperature: 0,
   });
 
-  console.log(completion.choices[0].message.content);
-}
+  const answer = response.choices[0]?.message.content.trim(); // react or node
+  if (answer === "react") {
+    res.json({
+      prompts: [
+        BASE_PROMPT,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [reactBasePrompt],
+    });
+    return;
+  }
 
-main();
+  if (answer === "node") {
+    res.json({
+      prompts: [
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+      ],
+      uiPrompts: [nodeBasePrompt],
+    });
+    return;
+  }
+
+  res.status(403).json({ message: "You can't access this" });
+  return;
+});
+
+app.post("/chat", async (req, res) => {
+  const messages = req.body.messages;
+  const response = await openai.chat.completions.create({
+    model: "qwen/qwen3-30b-a3b:free",
+    messages: messages,
+    temperature: 0,
+  });
+
+  console.log(response);
+
+  res.json({
+    response: response.choices[0].message.content,
+  });
+});
+
+app.listen(3000);
--- bilt/src/pages/Home.tsx
+++ bilt/src/pages/Home.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Wand2 } from "lucide-react";
-import axios from "axios";
-import { BACKEND_URL } from "../config";
 
 export function Home() {
   const [prompt, setPrompt] = useState("");

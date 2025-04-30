d60a7d9224c21740dc763b5b3c432ee8039dd11e
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2025-04-30 14:53:19 +0530

    creating frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+
+import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from './pages/Home';
 import { Builder } from "./pages/Builder";
 import { parseXml } from "./steps";
 function App() {

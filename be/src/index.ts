2716dfed3da28a967c06b3235a10420bf5a7931f
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2025-04-30 12:24:15 +0530

    added engineered prompt

--- be/src/index.ts
+++ be/src/index.ts
@@ -2,9 +2,8 @@ require("dotenv").config();
 import express from "express";
 import OpenAI from "openai";
 import cors from "cors";
-import { BASE_PROMPT, getSystemPrompt } from "./prompts";
-import { basePrompt as nodeBasePrompt } from "./defaults/node";
-import { basePrompt as reactBasePrompt } from "./defaults/react";
+import { BASE_PROMPT } from "./prompts";
+import { getSystemPrompt } from "./prompts";
 
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
@@ -15,59 +14,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.post("/template", async (req, res) => {
-  const prompt = req.body.prompt;
+async function main() {
+  const userMessage = "Write a code for todo application";
+  const systemPrompt = getSystemPrompt();
+  const fullPrompt = `${BASE_PROMPT}\n${systemPrompt}\nUser Message: ${userMessage}`;
 
-  const response = await openai.chat.completions.create({
+  const completion = await openai.chat.completions.create({
     model: "qwen/qwen3-30b-a3b:free",
     messages: [
       {
         role: "user",
-        content: prompt,
+        content: fullPrompt,
       },
     ],
     temperature: 0,
   });
+  console.log(completion.choices[0].message.content);
+}
 
-  const answer = response.choices[0]?.message.content.trim(); // react or node
-  if (answer === "react") {
-    res.json({
-      prompts: [
-        BASE_PROMPT,
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
-      ],
-      uiPrompts: [reactBasePrompt],
-    });
-    return;
-  }
-
-  if (answer === "node") {
-    res.json({
-      prompts: [
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${nodeBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
-      ],
-      uiPrompts: [nodeBasePrompt],
-    });
-    return;
-  }
-
-  res.status(403).json({ message: "You can't access this" });
-  return;
-});
-
-app.post("/chat", async (req, res) => {
-  const messages = req.body.messages;
-  const response = await openai.chat.completions.create({
-    model: "qwen/qwen3-30b-a3b:free",
-    messages: messages,
-    temperature: 0,
-  });
-
-  console.log(response);
-
-  res.json({
-    response: response.choices[0].message.content,
-  });
-});
-
-app.listen(3000);
+main();

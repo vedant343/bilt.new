25a78f39e2dfd3c9fa27d9cdd8c0cbe3c72c346e
Vedant Bhalerao <vedantbhalerao343@gmail.com>
2025-04-30 15:39:49 +0530

    making builder

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Builder } from "./pages/Builder";
-///import { parseXml } from "./steps";
 
 function App() {
   return (
